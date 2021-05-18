4c16cad0c17ccbac4decfba510c5f0158b4c08b9
chris1engel <81554629+chris1engel@users.noreply.github.com>
2021-05-18 17:13:02 +0200

    Loginfenster hinzugefügt
    Loginfenster hinzugefügt
    MAV.Helper PresentationCore.dll in den Ordner verschoben um das Hinzufügen zu vermeiden. Logo hinzugefügt. (Musste in den selben Ordner wie das Fenster. Wollte nicht anders)

---
+++ Programm/MAV/MAV.Login/Login.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MAV.Login
+{
+    /// <summary>
+    /// Interaktionslogik für Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+
+            InitializeComponent();
+        }
+
+        private void OnWindowClose(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OnWindowMinimize(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void DockPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        
+    }
+}
