077ba2d4fd286c01045b1093c46e133c3564b08d
chris1engel <81554629+chris1engel@users.noreply.github.com>
2021-05-18 17:23:04 +0200

    Updated "Any CPU"

--- Programm/MAV/MAV.Client/obj/Debug/netcoreapp3.1/MVVM/View/AdministrationView.g.i.cs
+++ Programm/MAV/MAV.Client/obj/Debug/netcoreapp3.1/MVVM/View/AdministrationView.g.i.cs
@@ -53,7 +53,7 @@ namespace MAV.Client.MVVM.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MAV.Client;V1.0.0.0;component/mvvm/view/administrationview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MAV.Client;component/mvvm/view/administrationview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\MVVM\View\AdministrationView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
