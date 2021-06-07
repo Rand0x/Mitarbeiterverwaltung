2db8bdaf7f5fade5195161550832b113a7319d5f
kaiserto84660 <kaiserto84660@th-nuernberg.de>
2021-06-07 18:01:15 +0200

    Merge fix

--- Programm/MAV/MAV.Login/Login.xaml.cs
+++ Programm/MAV/MAV.Login/Login.xaml.cs
@@ -19,7 +19,7 @@ namespace MAV.Login
     {
         public Login()
         {
-
+            this.DataContext = new LoginViewModel(this);
             InitializeComponent();
             UsernameBox.Focus();
         }
@@ -40,6 +40,10 @@ namespace MAV.Login
                 this.DragMove();
         }
 
-        
-    }
+        private void PasswordBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                ((LoginViewModel)this.DataContext).LogInCommand.Execute(null);
+        }
+  }
 }
