7e32eb3693e858e6365b06b3417193da9af4bfa9
kaiserto84660 <kaiserto84660@th-nuernberg.de>
2021-06-07 18:51:46 +0200

    DB merge fix

--- Programm/MAV/MAV.Login/obj/Debug/netcoreapp3.1/Login.g.i.cs
+++ Programm/MAV/MAV.Login/obj/Debug/netcoreapp3.1/Login.g.i.cs
@@ -93,7 +93,7 @@ namespace MAV.Login {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MAV.Login;component/login.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MAV.Login;V1.0.0.0;component/login.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Login.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
