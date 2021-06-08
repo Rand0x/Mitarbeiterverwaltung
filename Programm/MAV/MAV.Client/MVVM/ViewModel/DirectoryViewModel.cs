3c8beeb22185b53076091cada837e27cbe3134af
manuelroehrer <82162786+manuelroehrer@users.noreply.github.com>
2021-06-08 04:52:54 +0200

    Statische User fürs Directory
    Statische variable für den User, welcher im Directory ausgewählt wird. Wird benötigt für InfoView und EditView

--- Programm/MAV/MAV.Client/MVVM/ViewModel/DirectoryViewModel.cs
+++ Programm/MAV/MAV.Client/MVVM/ViewModel/DirectoryViewModel.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using MAV.Base;
+using MAV.DirectoryModule.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MAV.Client.MVVM.ViewModel
 {
-    class DirectoryViewModel
+    public class DirectoryViewModel : PropertyChangedBase
     {
+        private static User userSelected = new User();
+        public static User UserSelected
+        {
+            get
+            {
+                return userSelected;
+            }
+            set
+            {
+                userSelected = value;
+            }
+        }
+
+        private static User userSelectedEdited = new User();
+        public static User UserSelectedEdited
+        {
+            get
+            {
+                return userSelectedEdited;
+            }
+            set
+            {
+                userSelectedEdited = value;
+            }
+        }
     }
 }
